77751c5ebc3de6ba867bbe828b0044f5bd1e27af
mauran <maurakiri@gmail.com>
2018-04-25 20:19:11 +0530

    chnages

--- view.php
+++ view.php
@@ -1,6 +1,12 @@
 <html>
    <head>
       <title>Add Item</title>
+
+       <script>
+           function myFunction() {
+               window.open("https://www.w3schools.com");
+           }
+       </script>
    </head>
 
    <body>
@@ -13,20 +19,56 @@
 
       if ($result->num_rows > 0) {
           // output data of each row
-          while($row = $result->fetch_assoc()) {
+          while ($row = $result->fetch_assoc()) {
 
-              echo "<table>";
-              echo "<tr>";
-              echo "<td>Name</td>";
-              echo "<td>". $row["name"]."</td>";
-              echo "</tr>";
-              echo "</table>";
-           //   echo "<br> id: ". $row["title"]. " - Name: ". $row["image"]. "title " . $row["title"] . "<br>";
-          /*    echo "img src='".$row['image']."' width='175' height='200' />";*/
-              echo '<img width="100" height="100" src="data:image/jpeg;base64,'. $row['image'] .'"/>';
 
-          }
-      } else
+                  echo "<table border='1px'>";
+                  echo "<tr>";
+
+                  echo '<td><img width="100" height="100" src="data:image/jpeg;base64,' . $row['image'] . '"/></td>';
+
+
+                  /*  echo "<td>Name</td>";*/
+                  echo "<td>";
+                  echo "<table border='1px'>";
+                  echo "<tr>";
+                  echo "<td>Type</td><td> " . $row['type'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Title</td><td> " . $row['title'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Description</td><td> " . $row['description'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Name</td><td> " . $row['name'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Phone</td><td> " . $row['phone'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+                  echo "<td>Email</td><td> " . $row['email'] . "</td>";
+                  echo "</tr>";
+                  echo "<tr>";
+
+
+                  echo "</table >";
+
+                  echo "</td>";
+                  echo "</tr>";
+                  echo "</table>";
+
+                  echo "<br>";
+                  /*   echo "<button onclick=\"myFunction()\">view profile</button>";
+
+                     echo "<a href=\"http://google.com\" title=" . $row["name"]. "" . $row["phone"]. " >Google!</a>";
+                     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. "<br> " . $row["lastname"]. "<br>";*/
+                  echo "<br>";
+
+
+              }
+          
+      }else
       $conn->close();
       ?>
     <?php
