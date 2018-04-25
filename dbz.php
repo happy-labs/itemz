99cda799860ea1e4d187a8c2361cf0eab25b0008
eranga bandara <eranga.herath@pagero.com>
2018-04-25 07:59:16 +0530

    init itemz

---
+++ dbz.php
@@ -0,0 +1,8 @@
+<?php
+$connect=mysqli_connect(getenv('DB_HOST'), 'root', 'root', 'itemz');
+
+if(mysqli_connect_errno($connect)) {
+    echo 'Failed to connect db';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+   <head>
+      <title>Add Item</title>
+   </head>
+
+   <body>
+      <?php include 'dbz.php'; ?>
+      <?php
+         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $type = format_input($_POST["type"]);
+            $title = format_input($_POST["title"]);
+            $description = format_input($_POST["description"]);
+            $name = format_input($_POST["name"]);
+            $phone = format_input($_POST["phone"]);
+            $email = format_input($_POST["email"]);
+
+            // save itme db
+            mysqli_query($connect, "INSERT INTO item(type, title, description, name, phone, email)
+                VALUES('$type', '$title', '$description', '$name', '$phone', '$email')");
+         }
+
+         function format_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+         }
+      ?>
+
+      <h2>New Item</h2>
+      <form method = "post" action="<?=$_SERVER['PHP_SELF']?>">
+         <table>
+            <tr>
+               <td>Type:</td>
+               <td><input type = "text" name = "type"></td>
+            </tr>
+            <tr>
+               <td>Title:</td>
+               <td><input type = "text" name = "title"></td>
+            </tr>
+            <tr>
+               <td>Description:</td>
+               <td><textarea name = "description" rows = "5" cols = "40"></textarea></td>
+            </tr>
+            <tr>
+               <td>Image(browse):</td>
+            </tr>
+            <tr>
+               <td>Name:</td>
+               <td><input type = "text" name = "name"></td>
+            </tr>
+            <tr>
+               <td>Phone:</td>
+               <td><input type = "text" name = "phone"></td>
+            </tr>
+            <tr>
+               <td>Email:</td>
+               <td><input type = "text" name = "email"></td>
+            </tr>
+            <tr>
+               <td>
+                  <input type = "submit" name = "submit" value = "Submit">
+               </td>
+            </tr>
+         </table>
+      </form>
+      <?php
+      ?>
+   </body>
+</html>
