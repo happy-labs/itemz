3006e1a93e54366bf4d8b723f21f0a217c1b4b8f
mauran <maurakiri@gmail.com>
2018-04-26 14:47:56 +0530

    changes

--- view.php
+++ view.php
@@ -4,7 +4,21 @@
 
     <script>
         function myFunction() {
-            window.open("https://www.w3schools.com");
+            var input, filter, table, tr, td, i;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
         }
     </script>
 </head>
@@ -13,6 +27,7 @@
 <?php include 'dbz.php'; ?>
 <?php
 echo "  <h2> Item View</h2>";
+echo "<input type=\"text\" id=\"myInput\" onkeyup=\"myFunction()\" placeholder=\"Search for names..\" title=\"Type in a name\">";
 $sql = "SELECT type, title, description, name, phone, email, image FROM item";
 
 $result = $connect->query($sql);
@@ -20,7 +35,7 @@ $result = $connect->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo "<table border='1px'>";
+        echo "<table border='1px' id=\"myTable\">";
         echo "<tr>";
         echo '<td><img width="100" height="100" src="data:image/jpeg;base64,' . $row['image'] . '"/></td>';
 
